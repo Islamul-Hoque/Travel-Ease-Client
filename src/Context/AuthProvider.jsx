dccb57061e2f72999e6489dd1a768152ad6a8250
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-11-09 01:12:15 +0600

    Implemented Firebase authentication context with user state management

---
+++ src/Context/AuthProvider.jsx
@@ -0,0 +1,67 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../Firebase/firebase.config';
+
+export const AuthContext = createContext(null)
+
+const googleProvider = new GoogleAuthProvider()
+googleProvider.addScope('email') 
+
+const AuthProvider = ( {children} ) => {
+    const [ user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    const createUser = ( email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword( auth, email, password)
+    }
+
+    const signInUser = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    }
+
+    const signOutUser = () => {
+        setLoading(true)
+        return signOut(auth)
+    }
+
+    const googleSignIn = () =>{
+        setLoading(true)
+        return signInWithPopup( auth, googleProvider )
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if(currentUser) {
+                const email = currentUser.email || currentUser.providerData?.[0]?.email;
+                setUser({ ...currentUser, email });
+                setLoading(false);
+            } 
+            else {
+                setUser(null);
+            }
+            setLoading(false);
+        });
+    return () => unsubscribe();
+}, []);
+
+    const userInfo = {
+        user,
+        setUser,
+        loading,
+        setLoading,
+        createUser,
+        signInUser,
+        googleSignIn,
+        signOutUser,
+    }
+
+    return (
+        <AuthContext.Provider value={userInfo}> 
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
