77fb1776e538cb745f187d62b4b49103a0a48fe6
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-11-09 14:00:39 +0600

    Incorporated a responsive Footer for consistent layout and improved user accessibility

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -9,7 +9,7 @@ const RootLayout = () => {
         <div className='flex flex-col min-h-screen'>
             <header> <Navbar/> </header>
             <main className='bg-gray-100 grow'> <Outlet/> </main>
-            {/* <footer> <Footer/> </footer> */}
+            <footer> <Footer/> </footer>
         </div>
     );
 };
