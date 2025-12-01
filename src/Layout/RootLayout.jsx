98e65dafdb8ebb250b983f74e7b96f3ab0f0a7c1
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-12-01 12:42:59 +0600

    fix(layout): apply sticky header wrapper for Navbar to ensure top-0 positioning

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -7,7 +7,7 @@ import { Outlet } from 'react-router';
 const RootLayout = () => {
     return (
         <div className='flex flex-col min-h-screen'>
-            <header> <Navbar/> </header>
+            <header className='sticky top-0 z-100'> <Navbar/> </header>
             <main className='grow'> <Outlet/> </main>
             <footer> <Footer/> </footer>
         </div>
